28eb8d03d84ccfb3f4dfaed346210ea08572cbe5
lzhchn0 <136823672+lzhchn0@users.noreply.github.com>
2024-09-04 11:31:01 -0400

    Add files via upload

---
+++ scenario-code-gen/Monitoring.go
@@ -0,0 +1,28 @@
+package v1
+
+import (
+    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type Monitoring struct {
+    metav1.TypeMeta   `json:",inline"`
+    metav1.ObjectMeta `json:"metadata,omitempty"`
+
+    Spec   MonitoringSpec   `json:"spec,omitempty"`
+    Status MonitoringStatus `json:"status,omitempty"`
+}
+
+type MonitoringSpec struct {
+    Field1 string `json:"field1,omitempty"`
+    Field2 NestedField `json:"field2,omitempty"`
+}
+
+type NestedField struct {
+    SubField1 string `json:"subField1,omitempty"`
+    SubField2 string `json:"subField2,omitempty"`
+}
+
+type MonitoringStatus struct {
+    Condition string `json:"condition,omitempty"`
+}
